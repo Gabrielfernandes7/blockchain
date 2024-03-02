603fa8cd890fb255d30b77e99e2f2de2df77f736
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2024-03-02 13:17:19 -0300

    (feat): create blockchain

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ class Block:
         return sha.hexdigest()
     
 # cadeia de blocos ou blockchain
-
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
@@ -29,3 +28,18 @@ class Blockchain:
     def create_genesis_block(self):
         return Block(0, date.datetime.now(), "Genesis block", "0")
 
+    def add_block(self, new_block):
+        new_block.previous_hash = self.chain[-1].hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+            return True
